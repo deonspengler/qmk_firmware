35a2498e28200d2a8dcdfa162bd37459e0b39d05
Deon Spengler <deon@spengler.co.za>
2020-05-01 23:48:16 +0200

    Initial keymap

---
+++ keyboards/handwired/adept42/keymaps/default/keymap.c
@@ -0,0 +1,17 @@
+#include "adept42.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT(
+        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,
+        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
+        KC_LCTL, KC_ENT, RESET,
+        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LGUI, KC_SPC, KC_RALT),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}

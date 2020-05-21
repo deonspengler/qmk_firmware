f0f42f1565126103d89e12c2f7a6a2402c14ba1d
Deon Spengler <deon@spengler.co.za>
2020-05-21 16:20:56 +0200

    Added RGB breathing animation

--- keyboards/handwired/adept42/config.h
+++ keyboards/handwired/adept42/config.h
@@ -29,6 +29,8 @@
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 6
 #define RGBLED_SPLIT { 3, 3 }
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_EFFECT_BREATHING
 
 /* Enable split usb detect for Sparkfun Pro Micro */
 #define SPLIT_USB_DETECT

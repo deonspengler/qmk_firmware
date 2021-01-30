3c0c8040b5de38d0b2c3783557a3ebc67991e92a
Deon Spengler <deon@spengler.co.za>
2021-01-30 02:18:16 +0200

    Removed shift cadet and moved zero to center right thumb key

--- keyboards/handwired/adept42/keymaps/micro/keymap.c
+++ keyboards/handwired/adept42/keymaps/micro/keymap.c
@@ -20,7 +20,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Qwerty
    * Enter = Enter when tapped and Alt + Ctrl when held
    * Space = Space when tapped and Super when held
-   * Shift = Parenthesis when tapped and Shift when held
    * Layer2 = Leader key when tapped and layer 2 when held
    * ,-----------------------------------------.                    ,-----------------------------------------.
    * | Esc  |  Q   |  W   |  E   |  R   |  T   |                    |  Y   |  U   |  I   |  O   |  P   | Bspc |
@@ -35,28 +34,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                          KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, \
      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
-     KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, \
+     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
                        KC_LCTL, LCA_T(KC_ENT), MO(_L1),      TD(LT_LEAD), LGUI_T(KC_SPC), KC_RALT \
   ),
 
   /*
    * Layer1
-   * RCtl = Zero when tapped and RCtl when held
    * ,-----------------------------------------.                    ,-----------------------------------------.
    * |  `   |  !   |  @   |  #   |  {   |  }   |                    |  =   |  7   |  8   |  9   |  /   |  -   |
    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
    * |  ~   |  ^   |  &   |  $   |  [   |  ]   |                    |  %   |  4   |  5   |  6   |  *   |  +   |
    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-   * |Shift |  \   |  |   |  _   |  <   |  >   |                    |  ,   |  1   |  2   |  3   |  .   |Shift |
+   * |  (   |  \   |  |   |  _   |  <   |  >   |                    |  ,   |  1   |  2   |  3   |  .   |  )   |
    * `-----------------------------------------|------.      ,------|-----------------------------------------'
-   *                             | LCtl |Enter |L1Held|      | RCtl |Space | RAlt |
+   *                             | LCtl |Enter |L1Held|      | RCtl |  0   | RAlt |
    *                             `--------------------'      `--------------------'
    */
   [_L1] = LAYOUT( \
      KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_LCBR, KC_RCBR,             KC_EQL, KC_7, KC_8, KC_9, KC_SLSH, KC_PMNS, \
      KC_TILD, KC_CIRC, KC_AMPR, KC_DLR, KC_LBRC, KC_RBRC,           KC_PERC, KC_4, KC_5, KC_6, KC_PAST, KC_PPLS, \
-     KC_TRNS, KC_BSLS, KC_PIPE, KC_UNDS, KC_LT, KC_GT,              KC_COMM, KC_1, KC_2, KC_3, KC_PDOT, KC_TRNS, \
-                             KC_TRNS, KC_TRNS, KC_TRNS,      RCTL_T(KC_0), KC_TRNS, KC_TRNS \
+     KC_LPRN, KC_BSLS, KC_PIPE, KC_UNDS, KC_LT, KC_GT,              KC_COMM, KC_1, KC_2, KC_3, KC_PDOT, KC_RPRN, \
+                             KC_TRNS, KC_TRNS, KC_TRNS,      KC_RCTL, KC_0, KC_TRNS \
   ),
 
   /*
@@ -77,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_L2] = LAYOUT( \
      KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, LGUI(KC_SPC),               KC_PSCR, KC_PGUP, KC_INS, KC_HOME, KC_VOLU, KC_DEL, \
      LALT(KC_SPC), KC_F5, KC_F6, KC_F7, KC_F8, LCTL(KC_ENT),        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_VOLD, KC_PAUS, \
-     KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,                 KC_SLCK, KC_PGDN, KC_CAPS, KC_END, KC_MUTE, KC_TRNS, \
+     KC_LSFT, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,                 KC_SLCK, KC_PGDN, KC_CAPS, KC_END, KC_MUTE, KC_RSFT, \
                              KC_TRNS, KC_TRNS, KC_LALT,      KC_TRNS, KC_TRNS, KC_TRNS \
   ),
 

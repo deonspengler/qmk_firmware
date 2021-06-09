b117e0d7db397ba4f7e8fd7693918dc210df6244
Deon Spengler <deon@spengler.co.za>
2021-06-09 21:30:25 +0200

    Update keymaps and added incognito makro

--- keyboards/handwired/adept42/keymaps/micro/keymap.c
+++ keyboards/handwired/adept42/keymaps/micro/keymap.c
@@ -43,17 +43,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.                    ,-----------------------------------------.
    * |  `   |  !   |  @   |  #   |  {   |  }   |                    |  =   |  7   |  8   |  9   |  /   |  -   |
    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-   * |  ~   |  ^   |  &   |  $   |  [   |  ]   |                    |  %   |  4   |  5   |  6   |  *   |  +   |
+   * |  ~   |  ^   |  &   |  $   |  (   |  )   |                    |  %   |  4   |  5   |  6   |  *   |  +   |
    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-   * |  (   |  \   |  |   |  _   |  <   |  >   |                    |  ,   |  1   |  2   |  3   |  .   |  )   |
+   * |  >   |  \   |  |   |  _   |  [   |  ]   |                    |  ,   |  1   |  2   |  3   |  .   |  <   |
    * `-----------------------------------------|------.      ,------|-----------------------------------------'
    *                             | LCtl |Enter |L1Held|      | RCtl |  0   | RAlt |
    *                             `--------------------'      `--------------------'
    */
   [_L1] = LAYOUT( \
-     KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_LCBR, KC_RCBR,             KC_EQL, KC_7, KC_8, KC_9, KC_SLSH, KC_PMNS, \
-     KC_TILD, KC_CIRC, KC_AMPR, KC_DLR, KC_LBRC, KC_RBRC,           KC_PERC, KC_4, KC_5, KC_6, KC_PAST, KC_PPLS, \
-     KC_LPRN, KC_BSLS, KC_PIPE, KC_UNDS, KC_LT, KC_GT,              KC_COMM, KC_1, KC_2, KC_3, KC_PDOT, KC_RPRN, \
+     KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_LCBR, KC_RCBR,             KC_EQL, KC_7, KC_8, KC_9, KC_SLSH, KC_MINS, \
+     KC_TILD, KC_CIRC, KC_AMPR, KC_DLR, KC_LPRN, KC_RPRN,           KC_PERC, KC_4, KC_5, KC_6, KC_PAST, KC_PPLS, \
+     KC_GT, KC_BSLS, KC_PIPE, KC_UNDS, KC_LBRC, KC_RBRC,            KC_COMM, KC_1, KC_2, KC_3, KC_PDOT, KC_LT, \
                              KC_TRNS, KC_TRNS, KC_TRNS,      KC_RCTL, KC_0, KC_TRNS \
   ),
 
@@ -82,9 +82,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
    * Layer3
    * ,-----------------------------------------.                    ,-----------------------------------------.
-   * | Esc  |  V   |  Q   |  W   |  E   |  R   |                    |      |      |      |      |      |      |
+   * | Esc  |  V   |  Q   |  W   |  E   |  R   |                    |      |      |  Up  |      |      |      |
    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-   * | Tab  |Shift |  A   |  S   |  D   |  F   |                    |      |      |      |      |      |      |
+   * | Tab  |Shift |  A   |  S   |  D   |  F   |                    |      | Left | Down |Right |      |      |
    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
    * |  B   | LCtl |  Z   |  X   |  T   | LAlt |                    |      |      |      |      |      |TGLYR3|
    * `-----------------------------------------|------.      ,------|-----------------------------------------'
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                             `--------------------'      `--------------------'
    */
   [_L3] = LAYOUT( \
-     KC_ESC, KC_V, KC_Q, KC_W, KC_E, KC_R,                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-     KC_TAB, KC_LSFT, KC_A, KC_S, KC_D, KC_F,                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+     KC_ESC, KC_V, KC_Q, KC_W, KC_E, KC_R,                          KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, \
+     KC_TAB, KC_LSFT, KC_A, KC_S, KC_D, KC_F,                       KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, \
      KC_B, KC_LCTL, KC_Z, KC_X, KC_T, KC_LALT,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TG(_L3), \
                                  KC_C, KC_SPC, KC_G,         KC_NO, KC_NO, KC_NO \
   ),
@@ -184,6 +184,16 @@ void matrix_scan_user(void) {
       register_code(KC_INS);
       unregister_code(KC_INS);
       unregister_code(KC_LSFT);
+    }
+
+    // chromium incognito window
+    SEQ_ONE_KEY(KC_P) {
+      register_code(KC_LCTL);
+      register_code(KC_LSFT);
+      register_code(KC_N);
+      unregister_code(KC_LCTL);
+      unregister_code(KC_LSFT);
+      unregister_code(KC_N);
     }
 
     // send alt + ctrl + del
